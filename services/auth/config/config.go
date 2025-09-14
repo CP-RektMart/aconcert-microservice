6bcefcd0a570fbe29749b93a8b6c7d7f659b8aa5
boomchanotai <boom9387@hotmail.com>
2025-09-14 23:32:50 +0700

    feat: add pkg

--- services/auth/config/config.go
+++ services/auth/config/config.go
@@ -1,1 +1,8 @@
 package config
+
+type AppConfig struct {
+	Name         string `env:"NAME"`
+	Port         int    `env:"PORT"`
+	Environment  string `env:"ENVIRONMENT"`
+	MaxBodyLimit int    `env:"MAX_BODY_LIMIT"`
+}
