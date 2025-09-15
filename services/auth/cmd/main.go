e4b0267a8e5ac579372387de4ea7ef3a4a1adc60
tin2003tin <tinsiriwid@gmail.com>
2025-09-15 23:01:38 +0700

    feat:setup grpc for event service

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -3,5 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Auth Service")
+	lis, err := net.Listen("tcp", addr)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	
+	// Register ProductService server
+	pb.RegisterProductServiceServer(grpcServer, &ProductServer{})
+
+	log.Println("gRPC server running on", addr)
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve gRPC: %v", err)
+	}	
 }
