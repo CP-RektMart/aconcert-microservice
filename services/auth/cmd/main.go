f937833231e4ad69595edd6ac1dfa49c02b3bb91
tin2003tin <tinsiriwid@gmail.com>
2025-09-15 23:03:12 +0700

    fix: main

--- services/auth/cmd/main.go
+++ services/auth/cmd/main.go
@@ -3,18 +3,5 @@ package main
 import "fmt"
 
 func main() {
-	lis, err := net.Listen("tcp", addr)
-	if err != nil {
-		log.Fatalf("failed to listen: %v", err)
-	}
-
-	grpcServer := grpc.NewServer()
-	
-	// Register ProductService server
-	pb.RegisterProductServiceServer(grpcServer, &ProductServer{})
-
-	log.Println("gRPC server running on", addr)
-	if err := grpcServer.Serve(lis); err != nil {
-		log.Fatalf("failed to serve gRPC: %v", err)
-	}	
+	fmt.Println("Auth Service")
 }
